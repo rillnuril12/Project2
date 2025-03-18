63f7977ae7e544c30ed159b98e0ba770769a16f3
rillnuril12 <nurilihsan26.com>
2025-03-18 21:03:12 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+// Tugas 1
+
+let saldoAwal = Number(prompt ('Masukkan Saldo Anda'))
+let saldoTambahan = Number(prompt ('Masukkan Saldo Tambahan'))
+const saldoAkhir = saldoAwal + saldoTambahan
+
+alert ('Saldo akhir Anda sebesar Rp.' + saldoAkhir)
